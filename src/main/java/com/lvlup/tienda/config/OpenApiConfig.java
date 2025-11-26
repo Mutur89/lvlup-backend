bea59d944e6e59dee5301bfbc366ac16ec61d007
Simon Villar <sim.villar@duocuc.cl>
2025-11-25 23:05:42 -0300

    Agregar configuración de seguridad JWT en OpenAPI para la API de e-commerce

--- src/main/java/com/lvlup/tienda/config/OpenApiConfig.java
+++ src/main/java/com/lvlup/tienda/config/OpenApiConfig.java
@@ -5,6 +5,9 @@ import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.License;
 import io.swagger.v3.oas.models.servers.Server;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.Components;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -19,6 +22,9 @@ public class OpenApiConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        // Nombre del esquema de seguridad
+        final String securitySchemeName = "bearerAuth";
+
         return new OpenAPI()
                 .info(new Info()
                         .title("LvlUp E-commerce API")
@@ -41,6 +47,18 @@ public class OpenApiConfig {
                         new Server()
                                 .url("https://api.lvlupgaming.cl")
                                 .description("Servidor de Producción (si aplica)")
-                ));
+                ))
+                // Configuración de seguridad JWT
+                .addSecurityItem(new SecurityRequirement().addList(securitySchemeName))
+                .components(new Components()
+                        .addSecuritySchemes(securitySchemeName,
+                                new SecurityScheme()
+                                        .name(securitySchemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("bearer")
+                                        .bearerFormat("JWT")
+                                        .description("Ingresa el token JWT obtenido del endpoint /login")
+                        )
+                );
     }
 }
